593fdfffe0981b59c62716696b585200e2c55c8f
Sovianum <aklyukvin@yandex.com>
2018-01-01 19:32:39 +0300

    minor fix

--- common/errors.go
+++ common/errors.go
@@ -5,5 +5,5 @@ import (
 )
 
 func GetTypeError(expected string, got interface{}) error {
-	return fmt.Errorf("Expected type %s, got %v", expected, got)
+	return fmt.Errorf("expected type %s, got %v", expected, got)
 }
