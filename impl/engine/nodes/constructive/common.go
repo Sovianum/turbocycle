fb8b2282f5ec441d2adf09095fe5d0897f1f23e8
Sovianum <aklyukvin@yandex.com>
2018-01-07 18:42:14 +0300

    add multiadder constructor

--- impl/engine/nodes/constructive/common.go
+++ impl/engine/nodes/constructive/common.go
@@ -1,6 +1,32 @@
 package constructive
 
-import "math"
+import (
+	"math"
+
+	"github.com/Sovianum/turbocycle/common"
+	"github.com/Sovianum/turbocycle/core/graph"
+)
+
+func NewMultiAdder() graph.ReduceNode {
+	var groupReduceFunc = func(curr float64, port graph.Port) (float64, error) {
+		var val = port.GetState().Value()
+		var floatVal, ok = val.(float64)
+
+		if !ok {
+			return 0, common.GetTypeError("float64", val)
+		}
+
+		return curr * floatVal, nil
+	}
+
+	var totalReduceFunc = func(curr, new float64) (float64, error) {
+		return curr + new, nil
+	}
+
+	return graph.NewReduceNode(
+		groupReduceFunc, totalReduceFunc, 1, 0,
+	)
+}
 
 func NormMassRate(massRate, massRate0, t, t0, p, p0 float64) float64 {
 	return massRate / massRate0 * math.Sqrt(t0/t) * p / p0
---
+++ impl/engine/nodes/constructive/common_test.go
@@ -0,0 +1,26 @@
+package constructive
+
+import (
+	"testing"
+
+	"github.com/Sovianum/turbocycle/core/graph"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMultiAdder(t *testing.T) {
+	var port1, port2, port3, port4 = graph.NewPort(), graph.NewPort(), graph.NewPort(), graph.NewPort()
+
+	port1.SetState(graph.NewNumberPortState(2))
+	port2.SetState(graph.NewNumberPortState(3))
+	port3.SetState(graph.NewNumberPortState(4))
+	port4.SetState(graph.NewNumberPortState(5))
+
+	var node = NewMultiAdder()
+	node.AddPortGroup(port1, port2)
+	node.AddPortGroup(port3, port4)
+
+	var err = node.Process()
+	assert.Nil(t, err)
+
+	assert.InDelta(t, 26, node.OutputPort().GetState().Value().(float64), 1e-7)
+}
