6aa344020728c3f8ea9977ed5f5450d1a2222d12
Sovianum <aklyukvin@yandex.com>
2018-01-07 20:41:23 +0300

    add payload

---
+++ impl/engine/nodes/constructive/payload.go
@@ -0,0 +1,99 @@
+package constructive
+
+import (
+	"github.com/Sovianum/turbocycle/common"
+	"github.com/Sovianum/turbocycle/core/graph"
+	"github.com/Sovianum/turbocycle/impl/engine/nodes"
+	"github.com/Sovianum/turbocycle/impl/engine/states"
+)
+
+type Payload interface {
+	graph.Node
+	nodes.RPMSource
+	nodes.PowerSource
+
+	NormRPM() float64
+	SetNormRPM(normRPM float64)
+
+	RPM() float64
+	Power() float64
+}
+
+func NewPayload(powerCharacteristic func(normRpm float64) float64, rpm0, power0 float64) Payload {
+	var result = &payload{
+		powerCharacteristic: powerCharacteristic,
+
+		rpm0:    rpm0,
+		power0:  power0,
+		normRpm: 1,
+	}
+	graph.AttachAllPorts(
+		result,
+		&result.rpmOutput, &result.powerOutput,
+	)
+	return result
+}
+
+type payload struct {
+	*graph.BaseNode
+
+	rpmOutput   graph.Port
+	powerOutput graph.Port
+
+	rpm0   float64
+	power0 float64
+
+	powerCharacteristic func(normRpm float64) float64
+
+	normRpm float64
+}
+
+func (node *payload) NormRPM() float64 {
+	return node.normRpm
+}
+
+func (node *payload) SetNormRPM(normRPM float64) {
+	node.normRpm = normRPM
+}
+
+func (node *payload) RPM() float64 {
+	return node.rpm0 * node.normRpm
+}
+
+func (node *payload) Power() float64 {
+	return node.powerCharacteristic(node.normRpm) * node.power0
+}
+
+func (node *payload) GetName() string {
+	return common.EitherString(node.GetInstanceName(), "Payload")
+}
+
+func (node *payload) Process() error {
+	var rpm = node.normRpm * node.rpm0
+	var power = node.powerCharacteristic(node.normRpm) * node.power0
+
+	node.rpmOutput.SetState(states.NewRPMPortState(rpm))
+	node.powerOutput.SetState(states.NewPowerPortState(power))
+
+	return nil
+}
+
+func (node *payload) GetRequirePorts() []graph.Port {
+	return make([]graph.Port, 0)
+}
+
+func (node *payload) GetUpdatePorts() []graph.Port {
+	return []graph.Port{node.rpmOutput, node.powerOutput}
+}
+
+func (node *payload) GetPorts() []graph.Port {
+	return []graph.Port{node.rpmOutput, node.powerOutput}
+}
+
+func (node *payload) RPMOutput() graph.Port {
+	return node.rpmOutput
+}
+
+func (node *payload) PowerOutput() graph.Port {
+	return node.powerOutput
+}
---
+++ impl/engine/nodes/constructive/payload_test.go
@@ -0,0 +1,52 @@
+package constructive
+
+import (
+	"testing"
+
+	"github.com/Sovianum/turbocycle/core/math/solvers/newton"
+	"github.com/Sovianum/turbocycle/core/math/variator"
+	"github.com/stretchr/testify/assert"
+	"gonum.org/v1/gonum/mat"
+)
+
+const (
+	power0 = 1e6
+)
+
+func TestPayload_Process_Linear(t *testing.T) {
+	var f = func(normRpm float64) float64 {
+		return 1 - (normRpm-1)*1e5
+	}
+
+	var node = NewPayload(f, rpm0, power0)
+
+	var normRpm = 0.5
+	node.SetNormRPM(normRpm)
+	var err = node.Process()
+	assert.Nil(t, err)
+
+	assert.InDelta(t, power0*f(normRpm), node.PowerOutput().GetState().Value().(float64), 1e-7)
+	assert.InDelta(t, rpm0*normRpm, node.RPMOutput().GetState().Value().(float64), 1e-7)
+}
+
+func TestPayload_Process_Linear_Solver(t *testing.T) {
+	var f = func(normRpm float64) float64 {
+		return normRpm
+	}
+
+	var node = NewPayload(f, rpm0, power0)
+
+	var v = variator.FromCallables(node.NormRPM, node.SetNormRPM)
+	var systemCall = func() (*mat.VecDense, error) {
+		node.Process()
+		return mat.NewVecDense(1, []float64{node.Power() - power0/2}), nil
+	}
+
+	var solverGen = newton.NewUniformNewtonSolverGen(1e-4)
+	var variatorSolver = variator.NewVariatorSolver(systemCall, []variator.Variator{v}, solverGen)
+
+	var _, err = variatorSolver.Solve(mat.NewVecDense(1, []float64{1}), 1e-8, 100)
+	assert.Nil(t, err)
+
+	assert.InDelta(t, rpm0/2, node.RPM(), 1e-7)
+}
