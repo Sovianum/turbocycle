90c510dfca2cdee9b05bc7981e5afdf75245829a
Sovianum <aklyukvin@yandex.com>
2018-01-15 00:16:47 +0300

    add ch4 combustion test

--- material/fuel/fuel_test.go
+++ material/fuel/fuel_test.go
@@ -1,1 +1,13 @@
 package fuel
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCh4_GetCombustionGas(t *testing.T) {
+	var fuel = GetCH4()
+	var gas1 = fuel.GetCombustionGas(1)
+	assert.InDelta(t, 0, gas1.OxygenMassFraction(), 1e-7)
+}
