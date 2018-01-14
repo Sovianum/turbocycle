04ae13905b79cf78dfa863109fb615d343ce8870
Sovianum <aklyukvin@yandex.com>
2018-01-14 20:04:31 +0300

    test burn scheme

--- library/parametric/free3n/free3n_burn.go
+++ library/parametric/free3n/free3n_burn.go
@@ -1,1 +1,103 @@
 package free3n
+
+import (
+	"github.com/Sovianum/turbocycle/core/graph"
+	"github.com/Sovianum/turbocycle/core/math/variator"
+	c "github.com/Sovianum/turbocycle/impl/engine/nodes/constructive"
+	"github.com/Sovianum/turbocycle/impl/engine/nodes/sink"
+	"github.com/Sovianum/turbocycle/impl/engine/nodes/source"
+	"github.com/Sovianum/turbocycle/material/gases"
+)
+
+type ThreeShaftBurnFreeScheme interface {
+	ThreeShaftFreeScheme
+	MidBurner() c.ParametricBurnerNode
+}
+
+func NewThreeShaftBurnFreeScheme(
+	gas gases.Gas, tAtm, pAtm, tGas, tGasMid float64,
+	mpc c.ParametricCompressorNode, mpcPipe c.PressureLossNode,
+	mpt c.ParametricTurbineNode, mptPipe c.PressureLossNode,
+	mpEtaM float64,
+
+	hpc c.ParametricCompressorNode, hpcPipe c.PressureLossNode,
+	hpt c.ParametricTurbineNode, hptPipe c.PressureLossNode,
+	hpEtaM float64,
+
+	lpt c.ParametricTurbineNode, lptPipe c.PressureLossNode,
+	burner c.ParametricBurnerNode, payload c.Payload,
+	midBurner c.ParametricBurnerNode,
+) ThreeShaftBurnFreeScheme {
+	var root = NewThreeShaftFreeScheme(
+		gas, tAtm, pAtm, tGas,
+		mpc, mpcPipe, mpt, mptPipe, mpEtaM,
+		hpc, hpcPipe, hpt, hptPipe, hpEtaM,
+		lpt, lptPipe, burner, payload,
+	)
+
+	var result = &threeShaftBurnFreeScheme{
+		threeShaftFreeScheme: root.(*threeShaftFreeScheme),
+		midBurner:            midBurner,
+		midBurnerTSource:     source.NewTemperatureSourceNode(tGasMid),
+	}
+	result.relink()
+	result.resetEquations()
+
+	return result
+}
+
+type threeShaftBurnFreeScheme struct {
+	*threeShaftFreeScheme
+
+	midBurner        c.ParametricBurnerNode
+	midBurnerTSource source.TemperatureSourceNode
+
+	midBurnerEq graph.ReduceNode
+}
+
+func (scheme *threeShaftBurnFreeScheme) MidBurner() c.ParametricBurnerNode {
+	return scheme.midBurner
+}
+
+func (scheme *threeShaftBurnFreeScheme) GetNetwork() (graph.Network, error) {
+	return graph.NewNetwork(append(scheme.getNodes(), scheme.midBurner, scheme.midBurnerEq, scheme.midBurnerTSource))
+}
+
+func (scheme *threeShaftBurnFreeScheme) relink() {
+	graph.LinkAll(
+		[]graph.Port{
+			scheme.hptPipe.GasOutput(), scheme.hptPipe.TemperatureOutput(),
+			scheme.hptPipe.PressureOutput(), scheme.hptPipe.MassRateOutput(),
+		},
+		[]graph.Port{
+			scheme.midBurner.GasInput(), scheme.midBurner.TemperatureInput(),
+			scheme.midBurner.PressureInput(), scheme.midBurner.MassRateInput(),
+		},
+	)
+	graph.LinkAll(
+		[]graph.Port{
+			scheme.midBurner.GasOutput(), scheme.midBurner.TemperatureOutput(),
+			scheme.midBurner.PressureOutput(),
+		},
+		[]graph.Port{
+			scheme.mpShaft.Turbine.GasInput(), scheme.mpShaft.Turbine.TemperatureInput(),
+			scheme.mpShaft.Turbine.PressureInput(),
+		},
+	)
+	sink.SinkAll(
+		scheme.midBurner.MassRateOutput(), scheme.mpShaft.Turbine.MassRateInput(),
+		scheme.midBurnerTSource.TemperatureOutput(),
+	)
+}
+
+func (scheme *threeShaftBurnFreeScheme) resetEquations() {
+	scheme.variators = append(
+		scheme.variators,
+		variator.FromCallables(scheme.midBurner.FuelRateRel, scheme.midBurner.SetFuelRateRel),
+	)
+	scheme.midBurnerEq = c.NewEquality(
+		graph.NewWeakPort(scheme.midBurner.TemperatureOutput()),
+		graph.NewWeakPort(scheme.midBurnerTSource.TemperatureOutput()),
+	)
+	scheme.assembler.AddInputPorts(scheme.midBurnerEq.OutputPort())
+}
--- library/parametric/free3n/free3n_burn_test.go
+++ library/parametric/free3n/free3n_burn_test.go
@@ -1,1 +1,155 @@
 package free3n
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Sovianum/turbocycle/core/math/solvers/newton"
+	"github.com/Sovianum/turbocycle/core/math/variator"
+	c "github.com/Sovianum/turbocycle/impl/engine/nodes/constructive"
+	"github.com/Sovianum/turbocycle/material/fuel"
+	"github.com/Sovianum/turbocycle/material/gases"
+	"github.com/stretchr/testify/assert"
+	"gonum.org/v1/gonum/mat"
+)
+
+const (
+	midBurnerTOut = 1100.
+)
+
+func TestNewTripleShaftBurnFreeScheme_Smoke(t *testing.T) {
+	var scheme = getUnit3nBurnFreeTestScheme()
+	var network, err = scheme.GetNetwork()
+	assert.Nil(t, err)
+
+	var converged = false
+	converged, err = network.Solve(1, 2, 100, 1e-3)
+
+	assert.True(t, converged)
+	assert.Nil(t, err)
+
+	var sysCall = variator.SysCallFromNetwork(
+		network, scheme.Assembler().GetVectorPort(), 1, 2, 100, 1e-3,
+	)
+	var variators = scheme.Variators()
+	var solverGen = newton.NewUniformNewtonSolverGen(1e-4)
+
+	var variatorSolver = variator.NewVariatorSolver(sysCall, variators, solverGen)
+
+	_, err = variatorSolver.Solve(
+		mat.NewVecDense(10, []float64{
+			1, 1, 1, 1,
+			1, 1, 1,
+			0.034, 1, 0.02,
+		}),
+		1e-8, 0.5, 100,
+	)
+	assert.Nil(t, err)
+
+	fmt.Println()
+	fmt.Println(scheme.LPT().PiTStag())
+	fmt.Println(scheme.MPC().PiStag(), scheme.MPT().PiTStag())
+	fmt.Println(scheme.HPC().PiStag(), scheme.HPT().PiTStag())
+	fmt.Println(scheme.Payload().RPM(), scheme.MPC().MassRate())
+	fmt.Println(scheme.MidBurner().Alpha())
+
+	var delta = 1e-7
+	assert.InDelta(
+		t,
+		scheme.MPC().MassRateOutput().GetState().Value().(float64),
+		scheme.HPC().MassRateInput().GetState().Value().(float64),
+		delta,
+	)
+	assert.InDelta(
+		t,
+		scheme.Burner().MassRateOutput().GetState().Value().(float64),
+		scheme.HPT().MassRateInput().GetState().Value().(float64),
+		delta,
+	)
+	assert.InDelta(
+		t,
+		scheme.HPT().MassRateOutput().GetState().Value().(float64),
+		scheme.MPT().MassRateInput().GetState().Value().(float64),
+		delta,
+	)
+	assert.InDelta(
+		t,
+		scheme.MPT().MassRateOutput().GetState().Value().(float64),
+		scheme.LPT().MassRateInput().GetState().Value().(float64),
+		delta,
+	)
+}
+
+func getUnit3nBurnFreeTestScheme() ThreeShaftBurnFreeScheme {
+	return get3nBurnFreeTestScheme(
+		func(normMassRate, normPiStag float64) float64 {
+			return 1
+		},
+		func(normMassRate, normPiStag float64) float64 {
+			return 1
+		},
+		func(normMassRate, normPiStag float64) float64 {
+			return 1
+		},
+		func(normMassRate, normPiStag float64) float64 {
+			return 1
+		},
+		func(lambdaU, normPiStag float64) float64 {
+			return 1
+		},
+		func(lambdaU, normPiStag float64) float64 {
+			return 1
+		},
+		func(lambdaU, normPiStag float64) float64 {
+			return 1
+		},
+		func(lambdaU, normPiStag float64) float64 {
+			return 1
+		},
+		func(lambdaU, normPiStag float64) float64 {
+			return 1
+		},
+		func(lambdaU, normPiStag float64) float64 {
+			return 1
+		},
+		func(lambda float64) float64 {
+			return 1
+		},
+		func(normRPM float64) float64 {
+			return normRPM * normRPM
+		},
+		func(lambda float64) float64 {
+			return 1
+		},
+	)
+}
+
+func get3nBurnFreeTestScheme(
+	mpcNormEtaChar, mpcNormRpmChar,
+	hpcNormEtaChar, hpcNormRpmChar c.CompressorCharFunc,
+	hptNormMassRateChar, hptNormEtaChar,
+	mptNormMassRateChar, mptNormEtaChar,
+	lptNormMassRateChar, lptNormEtaChar c.TurbineCharFunc,
+	burnerSigmaFunc func(lambda float64) float64,
+	powerChar func(normRPM float64) float64,
+	midBurnerSigmaFunc func(lambda float64) float64,
+) ThreeShaftBurnFreeScheme {
+	var root = get3nFreeTestScheme(
+		mpcNormEtaChar, mpcNormRpmChar,
+		hpcNormEtaChar, hpcNormRpmChar,
+		hptNormMassRateChar, hptNormEtaChar,
+		mptNormMassRateChar, mptNormEtaChar,
+		lptNormMassRateChar, lptNormEtaChar,
+		burnerSigmaFunc, powerChar,
+	)
+	return NewThreeShaftBurnFreeScheme(
+		gases.GetAir(), tAtm, pAtm, tGas, midBurnerTOut,
+		root.MPC(), root.MPCPipe(), root.MPT(), root.MPTPipe(), mpEtaM,
+		root.HPC(), root.HPCPipe(), root.HPT(), root.HPTPipe(), hpEtaM,
+		root.LPT(), root.LPTPipe(), root.Burner(),
+		root.Payload(), c.NewParametricBurnerNode(
+			fuel.GetCH4(), tFuel, t0, etaBurn, lambdaIn0, p0*mpcPi0, midBurnerTOut,
+			hpcMassRate0, fuelMassRateRel0, precision, midBurnerSigmaFunc,
+		),
+	)
+}
