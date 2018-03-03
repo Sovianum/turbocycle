b546ed5a1e403177651fea1b4c020814e35e5f31
Sovianum <aklyukvin@yandex.com>
2018-03-03 09:37:36 +0300

    fix norm mass rate calculation

--- impl/engine/nodes/constructive/common.go
+++ impl/engine/nodes/constructive/common.go
@@ -75,11 +75,16 @@ func NewMultiAdder() graph.ReduceNode {
 }
 
 func NormMassRate(massRate, massRate0, t, t0, p, p0 float64) float64 {
-	return massRate / massRate0 * math.Sqrt(t0/t) * p / p0
+	pRel := p / p0
+	tRel := t / t0
+
+	return massRate / massRate0 * math.Sqrt(tRel) / pRel
 }
 
 func MassRate(normMassRate, massRate0, t, t0, p, p0 float64) float64 {
-	return normMassRate / (math.Sqrt(t0/t) * p / p0) * massRate0
+	pRel := p / p0
+	tRel := t / t0
+	return normMassRate * pRel / math.Sqrt(tRel) * massRate0
 }
 
 func NormRpm(rpm, rpm0, t, t0 float64) float64 {
