2f28946a3f11e2b7e7267364989b334b6bb9bc79
Artem Klyukvin <aklyukvin@yandex.ru>
2018-04-10 13:59:52 +0300

    test compressor triangles

--- impl/stage/compressor/stage_node_test.go
+++ impl/stage/compressor/stage_node_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	states2 "github.com/Sovianum/turbocycle/impl/engine/states"
+	"github.com/Sovianum/turbocycle/impl/stage/geometry"
 	"github.com/Sovianum/turbocycle/impl/stage/states"
 	"github.com/Sovianum/turbocycle/material/gases"
 	"github.com/stretchr/testify/suite"
@@ -45,6 +46,10 @@ type StageNodeTestSuite struct {
 }
 
 func (suite *StageNodeTestSuite) SetupTest() {
+	suite.initNode(rpm)
+}
+
+func (suite *StageNodeTestSuite) initNode(rpmArg float64) {
 	suite.gen = NewStageGeometryGenerator(
 		dRelIn,
 		NewIncompleteGenerator(baRel, deltaRel, gammaIn, gammaOut),
@@ -55,7 +60,7 @@ func (suite *StageNodeTestSuite) SetupTest() {
 		dRelIn,
 		htCoef, htCoef,
 		reactivity, reactivity,
-		labourCoef, etaAd, caCoef, rpm,
+		labourCoef, etaAd, caCoef, rpmArg,
 		suite.gen, precision, relaxCoef, 0.99, iterLimit,
 	).(*stageNode)
 	suite.node.GasInput().SetState(states2.NewGasPortState(gases.GetAir()))
@@ -94,6 +99,32 @@ func (suite *StageNodeTestSuite) TestSmoke() {
 	//} else {
 	//	fmt.Println(string(b))
 	//}
+}
+
+func (suite *StageNodeTestSuite) TestTriangles() {
+	suite.initNode(rpm)
+	triangle11 := suite.pack.InletTriangle
+	triangle12 := suite.pack.OutletTriangle
+	geom1 := suite.pack.StageGeometry.StatorGeometry()
+	r13 := geometry.RRel(
+		geom1.InnerProfile().Diameter(geom1.XGapOut()) / geom1.OuterProfile().Diameter(geom1.XGapOut()),
+	)
+
+	suite.initNode(rpm * 0.6)
+	triangle21 := suite.pack.InletTriangle
+	triangle22 := suite.pack.OutletTriangle
+	geom2 := suite.pack.StageGeometry.StatorGeometry()
+	r23 := geometry.RRel(
+		geom2.InnerProfile().Diameter(geom2.XGapOut()) / geom2.OuterProfile().Diameter(geom2.XGapOut()),
+	)
+
+	suite.InDelta(triangle11.Alpha(), triangle21.Alpha(), 1e-9)
+	suite.InDelta(triangle11.Beta(), triangle21.Beta(), 1e-9)
+
+	suite.True(triangle12.CA() > triangle22.CA())
+	suite.InDelta(r13, r23, 1e-9)
+
+	suite.True(triangle12.Alpha() < triangle22.Alpha(), "%f, %f", triangle12.Alpha(), triangle22.Alpha())
 }
 
 func TestStageNodeTestSuite(t *testing.T) {
